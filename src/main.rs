ce7a52b6ced770f3fbd338d4b89d970bad959926
Abhraji-debnth <abhrajitdebnath.cs@gmail.com>
2026-03-15 12:12:59 +0530

    intial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+    let a: u32 = 787876;
+    println!("{a}");
+
+    let 
+
+    let result: i32 = snake_case();
+    println!("{result}");
+}
+
+fn snake_case() -> i32 {
+    2
+}
