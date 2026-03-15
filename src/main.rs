20c26e23c86701b90b100df3fa79747ef6718bf8
Abhraji-debnth <abhrajitdebnath.cs@gmail.com>
2026-03-15 15:51:11 +0530

    feat: add calculation feature based on user input with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,56 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    let a: u32 = 787876;
-    println!("{a}");
 
-    let 
+ // initialize input buffers
 
-    let result: i32 = snake_case();
-    println!("{result}");
+    let mut first_input = String::new();
+    let mut second_input = String::new();
+    let mut operator_input = String::new();
+
+    // read inputs from user
+
+    println!("Enter first number : ");
+    io::stdin()
+        .read_line(&mut first_input)
+        .expect("Failed to load");
+
+    println!("Enter  operator : ");
+    io::stdin()
+        .read_line(&mut operator_input)
+        .expect("Failed to load");
+    println!("Enter second number : ");
+    io::stdin()
+        .read_line(&mut second_input)
+        .expect("Failed to load");
+
+    // parse strings to integers
+
+    let num1: i32 = first_input.trim().parse().expect("enter valid number");
+    let operator = operator_input.trim().to_string();
+    let num2: i32 = second_input.trim().parse().expect("enter val;id number");
+
+   // calculate and print result
+
+    let result = cal_result(num1, operator, num2);
+    println!("{result}")
 }
 
-fn snake_case() -> i32 {
-    2
+
+   
+// calculates result based on operator
+
+fn cal_result(first_input: i32, operator: String, second_input: i32) -> i32 {
+    if operator == "+" {
+        first_input + second_input
+    } else if operator == "-" {
+        first_input - second_input
+    } else if operator == "*" {
+        first_input * second_input
+    } else if operator == "/" {
+        first_input / second_input
+    } else {
+        println!("Invalid Input");
+        0
+    }
 }
